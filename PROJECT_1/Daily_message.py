3f8834d1e89564b5ece33dd866e0920f5599248c
Tosinkuzzy <adeleye608@gmail.com>
2023-11-03 23:20:13 +0100

    First pycode

---
+++ PROJECT_1/Daily_message.py
@@ -0,0 +1,25 @@
+
+print("Welcome To Crownstar High Level Programming")
+Welcome To Crownstar High Level Programming
+>>> print("List Of Daily TODO List....:")
+List Of Daily TODO List....:
+>>> print("1. CONTRIBUTING TO PROJECT_1")
+1. CONTRIBUTING TO PROJECT_1
+>>> print("2. DEBUGGING TO PROJECT_1")
+2. DEBUGGING TO PROJECT_1
+>>> print("3. DEPLOYMENT TO PROJECT_1")
+3. DEPLOYMENT TO PROJECT_1
+>>> print("4. PUSH CODE TO PROJECT_1")
+4. PUSH CODE TO PROJECT_1
+>>> print("5. DAILY MOTIVATION FOR PROJECT_1")
+5. DAILY MOTIVATION FOR PROJECT_1
+>>> print(input("SELECTION: ")
+... 2
+  File "<stdin>", line 2
+    2
+    ^
+SyntaxError: invalid syntax
+>>> print(input("SELECTION: ")
+... if _ in selection(range(5):
+  File "<stdin>", line 2       
+    if _ in selection(range(5):
