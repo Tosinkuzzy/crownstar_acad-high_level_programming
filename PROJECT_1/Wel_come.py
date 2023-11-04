b227aab132c49d341cf1311bf180c8dd6063b489
Tosinkuzzy <adeleye608@gmail.com>
2023-11-04 19:56:45 +0100

    Project_1 Folder Updated

--- PROJECT_1/Wel_come.py
+++ PROJECT_1/Wel_come.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 
+# Welcome Message to Users.
 print("Welcome To Crownstar High Level Programming")
 
+# List of projects
+print("TODO List....")
+# Pick any of the project you are interested in!
+print("1. CODING ")
+print("2. CONTRIBUTING ")
+print("3. DEBUGGING ")
+print("4. DEPLOYMENT ")
+print("5. MAINTENANCE ")
 
-print("TODO List....:")
-
-print("1. CONTRIBUTING TO PROJECT_1")
-print("2. DEBUGGING TO PROJECT_1")
-print("3. DEPLOYMENT TO PROJECT_1")
-print("4. PUSH CODE TO PROJECT_1")
-print("5. DAILY MOTIVATION FOR PROJECT_1")
-
+# We have 5 project here now that need to be fixed.
+# 1. is that we need to implement some codes
+# whenever the user decide to input what they decide to do
+# like after displaying those list, the command should prompt for
+# input from the user to input his/her selection
+# then we will implement this so when they pick one or two we can get the
+# user information like, name, email, and social account where we can
+# follow each other up.
+# put in more ideas before implementations
