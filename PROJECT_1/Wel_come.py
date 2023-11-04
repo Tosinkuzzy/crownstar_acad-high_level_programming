5f34e607aa66ecad7da3f8fa26f0ec4f612c57cc
Tosinkuzzy <adeleye608@gmail.com>
2023-11-04 18:55:42 +0100

    welcome script

---
+++ PROJECT_1/Wel_come.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+print("Welcome To Crownstar High Level Programming")
+
+
+print("TODO List....:")
+
+print("1. CONTRIBUTING TO PROJECT_1")
+print("2. DEBUGGING TO PROJECT_1")
+print("3. DEPLOYMENT TO PROJECT_1")
+print("4. PUSH CODE TO PROJECT_1")
+print("5. DAILY MOTIVATION FOR PROJECT_1")
+
