10f742a9b53abfc296f02018b9b8d117934054c5
Beri <programmingwithkinyuyyurika@gmail.com>
2023-11-06 06:16:29 +0100

    base_hex.py

---
+++ base_hex.py
@@ -0,0 +1,8 @@
+# WAP that uses hex() to convert from any base to hexa decimal
+# If you look at my previous codes with bin() you will see that I used three other methods to come out with the same code. You can refer to the previous code and implement here.
+m = hex(100)
+n = hex(0B111111)
+o = hex(0o12345)
+print(m)
+print(n)
+print(o)
