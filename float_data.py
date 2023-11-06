fd24b34d623b5d9a010a20d3967e627107010a86
Beri <programmingwithkinyuyyurika@gmail.com>
2023-11-06 06:28:09 +0100

    float_data.py

---
+++ float_data.py
@@ -0,0 +1,10 @@
+# WAP that uses float values by using decimal form.
+# N.B We can represent int values in decimal, binary, octal and hexa decimal forms. BUT we can represent float values ONLY BY USING DECIMAL FORM.
+# That is why all the codes we will have below will all have errors.
+
+a = 0B11.01
+b = 0o123.456
+c = 0X123.456
+print(a)
+print(b)
+print(c)
