179ef079dafd5c6e02db3448c0abecb87a81d8ec
Beri <programmingwithkinyuyyurika@gmail.com>
2023-11-11 09:42:59 +0100

    assignment-operator.py

---
+++ assignment-operator.py
@@ -0,0 +1,14 @@
+''' Assignment Operators:
+a) We can use this to assign value to the variable. E.g x = 10
+b) We can combine assignment operator with some other operator to form compound assignment operator. 
+E.G x += 10 
+LIST OF POSSIBLE COMPOUND ASSIGNMENT OPERATORS IN PY
++=, -=, /=, %=, //=, **=, &=, |=, ^=, >>=, <<= '''
+
+x = 10
+x += 20
+print(x)
+
+x = 10
+x &= 5
+print(x)
