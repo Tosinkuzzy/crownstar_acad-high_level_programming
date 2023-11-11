3a5504011a67d4d2d50506c27282df0c82415ab0
Beri <programmingwithkinyuyyurika@gmail.com>
2023-11-11 09:31:19 +0100

    shift-operators.py

---
+++ shift-operators.py
@@ -0,0 +1,17 @@
+''' Shift operators:
+<< Left Shift Operators: After shifting the empty cells we have to fill with zero '''
+
+print(10 << 2)
+
+''' >> Right Shift Operators: After shifting the empty cells we have to fill with sign bit.(0 for +ve and 1 for -ve) '''
+
+print(10 >> 2)
+
+# We can apply bitwise operators for boolean types also
+
+print(True & False)
+print(True | False)
+print(True ^ False)
+print(~True)
+print(True << 2)
+print(True >> 2)
