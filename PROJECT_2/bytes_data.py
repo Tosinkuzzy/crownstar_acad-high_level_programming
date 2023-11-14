43f1c7bd6ea9301bde14c69c3ec91fe9a012f4aa
Tosinkuzzy <adeleye608@gmail.com>
2023-11-14 23:47:52 +0100

    this file has bug after when you run it, but documentations are there to fix the bugs.

--- PROJECT_2/bytes_data.py
+++ PROJECT_2/bytes_data.py
@@ -1,5 +1,6 @@
-# WAF that uses bytes data type to represent a group of byte numbers just like an array.
-# Write Python 3 code in this online editor and run it.
+""" WAF that uses bytes data type to represent a group of byte numbers just like an array.
+Write Python 3 code in this online editor and run it.
+"""
 x = [10, 20, 30, 40]
 b = bytes(x)
 type(b)
@@ -7,7 +8,7 @@ print(b[0])
 print(b[1])
 print(b[2])
 print(b[-1])
-i in b : print(i) # This line of code bring an error
+for i in b : print(i) # This line of code bring an error
 
 # Conclusion 1: The only allowed values for byte data type are 0 to 256. By mistake if we are trying to provide any other values then we will get value error.
 # Conclusion 2: Once we create bytes data type value, we cannot change its values, otherwise we will get TypeError. E.G
@@ -15,5 +16,5 @@ i in b : print(i) # This line of code bring an error
 x = [10, 20, 30, 40]
 b = bytes(x)
 b[0] = 100
-print(b[100]) # Output is TypeError: 'bytes' object does not upport item assignment
+print(b[1]) # Output is TypeError: 'bytes' object does not support item assignment
 
