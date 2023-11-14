c783ba5b9003bb274cb9b12c3c8adecf5fbd92d8
Tosinkuzzy <adeleye608@gmail.com>
2023-11-14 22:12:49 +0100

    relational operator file modified to chained.

--- PROJECT_2/relational-operators.py
+++ PROJECT_2/relational-operators.py
@@ -6,30 +6,33 @@ print("a >= b is ", a >= b)
 print("a < b is ", a < b)
 print("a <= b is ", a <= b)
 
-# Example 2
-a = "mangong"
-b = "mangong"
-print("a > b is ", a > b)
-print("a >= b is ", a >= b)
-print("a < b is ", a < b)
-print("a <= b is ", a <= b)
+# Project 2
+""" passing float to the variables,
+using if_elif_else chain to verify,
+relational operators. """
 
-# Example 3
-print(True > True)
-print(True >= True)
-print(10 > True)
-print(False > True)
-#print(10 > 'durga')
+a = 34.7
+b = 32.66
+if a > b:
+	print("Tosinkuzzy Payment Confirmed.")
+elif a >= b:
+	print("Order Pending Payment.")
+elif a < b:
+	print("Mangong Payment Confirmed.")
+else:
+	print("NO PENDING PAYMENT.")
 
-# Example 4
+# Project 3
 a = 10
 b = 20
-if( a > b):
+if(a > b):
     print("a is greater than b")
 else:
         print("a is not greater than b")
         
-''' N.B Chaining of relational operators is possibe. In the chaining, if all comparisons returns True then only result is True. If atleast one comparison returns False then the result is False '''
+"""N.B Chaining of relational operators is possibe. In the chaining,
+ if all comparisons returns True then only result is True. 
+If atleast one comparison returns False then the result is False. """
 print(10 < 20)
 print(10 < 20 < 30)
 print(10 < 20 < 30 < 40)
