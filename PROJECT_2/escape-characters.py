b7dff4233cefb7764f18356b74d052158ef5e3bd
Tosinkuzzy <adeleye608@gmail.com>
2023-11-15 00:16:47 +0100

    escape-characters bugs & documentations modified.

---
+++ PROJECT_2/escape-characters.py
@@ -0,0 +1,9 @@
+""" WAP where in string literals we can use escape characters to associate a special meaning """
+s = "mangong\nprogrammer"
+print(s)
+s = "Tosinkuzzy\tprogrammer"
+print(s)
+#s = "This is " crownstar" _> error
+s = "This is \" crownstar"
+print(s)
+
