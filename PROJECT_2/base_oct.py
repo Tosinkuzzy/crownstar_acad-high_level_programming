1888f3713df14a967f51cdf38b96f0df365bf412
Tosinkuzzy <adeleye608@gmail.com>
2023-11-14 22:45:49 +0100

    base_octal documentations modified.

--- PROJECT_2/base_oct.py
+++ PROJECT_2/base_oct.py
@@ -1,10 +1,9 @@
-"""  WAP that uses hex() to convert from any base to hexa decimal
+""" WAP that uses hex() to convert from any base to hexa decimal
 Here is the doc string i did, it can take so many strings as comment
-and docs
- """
-# you can also implement by using ***********________********* to design comment, to make your code looks like pro.
-""" If you look at my previous codes with bin() you will see that I used three other methods to come out with the same code.
- You can refer to the previous code and implement here.
+and docs.
+you can also implement by using ***********________********* to design comment, to make your code looks like pro.
+If you look at my previous codes with bin() you will see that I used three other methods to come out with the same code.
+You can refer to the previous code and implement here.
 """
 # your single comment shouldn't be that long ok. if you want to use a long one then implement double quotes
 m = hex(100)
