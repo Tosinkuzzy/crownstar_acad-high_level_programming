8f04b3799edf6d9b4ce1aaab9e420549f0ec4bc9
Tosinkuzzy <adeleye608@gmail.com>
2023-11-15 00:52:11 +0100

    fundamental-vs-immutability_data_types bugs & documentation modified.

--- PROJECT_2/fundamental-vs-immutability_data_types.py
+++ PROJECT_2/fundamental-vs-immutability_data_types.py
@@ -1,10 +1,20 @@
-# WAF that shows fundamental data types vs immutability. All fundamental data types are immutable. Ones we create an object, we cannot perform any changes in that object.
-# In py, if a new obj is required, the PVM won't create object immediatly. First it will check if any obj is available with required content. If any then existing obj will be reused. If it is not available then only a new obj will be created. The adv of this approach is memory utilization and performance will be improved.
-# But the prob in this approach is, several references pointing to the same object, by using one reference if we are allowed to change the content in the existing object then the remaining references will be effected. To prevent immutability concept is required. According to this once creates an object we are not allowed to change content. If we are trying to change with those changes a new object will be created.
-# Write Python 3 code in this online editor and run it.
+""" WAF that shows fundamental data types vs immutability.
+ All fundamental data types are immutable.
+ Ones we create an object, we cannot perform any changes in that object.
+ In py, if a new obj is required, the PVM{python virtual machine} won't create object immediatly.
+ First it will check if any obj is available with required content.
+ If any then existing obj will be reused.
+ If it is not available then only a new obj will be created.
+ The adv of this approach is memory utilization and performance will be improved.
+But the prob in this approach is, several references pointing to the same object,
+ by using one reference if we are allowed to change the content in the existing object then the remaining references will be affected.
+ To prevent immutability concept is required. 
+According to this, once creates an object we are not allowed to change content.
+ If we are trying to change with those changes a new object will be created.
+"""
 a = 10
 b = 10
-id(a)
+id(a)#_> to check obj identity
 id(b)
 a is b
 print(a)
@@ -34,13 +44,13 @@ print(b)
 print(id(a))
 print(id(b))
 
-a = 'Godson'
+a = 'Crownstar'
 b = 'Godson'
-a is b
+a is not b
 id(a)
 id(b)
 print(a)
 print(b)
-print(a is b)
+print(a is not b)
 print(id(a))
 print(id(b))
