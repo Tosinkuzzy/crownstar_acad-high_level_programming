85d4650f1113ff8454105152cd8d9bba58a63750
Tosinkuzzy <adeleye608@gmail.com>
2023-11-14 23:07:05 +0100

    bytearray-data_type bug, documentations modified.

--- PROJECT_2/bytearray-data_type.py
+++ PROJECT_2/bytearray-data_type.py
@@ -1,5 +1,5 @@
 # WAF that uses bytearray which is exactly the same as bytes data type except that its elements can be modified.
-# Example 1:
+# Project 1:
 x = [10, 20, 30, 40]
 b = bytearray(x)
 for i in b : print(i) # This line of code will bring an error
@@ -9,12 +9,12 @@ print(b[2])
 print(b[3])
 b[0] = 100
 for i in b : print(i) # This line of code will bring an error
-print(b[100])
+# print(b[100]) -> error
 print(b[1])
 print(b[2])
 print(b[3])
 
-# Example 2:
-x = [10,256]
+# Project 2:
+x = [10, 25]#_> try with a different integer
 b = bytearray(x)
-ValueError: byte must be in range(0, 256)
+#ValueError: byte must be in range(0, 256)
