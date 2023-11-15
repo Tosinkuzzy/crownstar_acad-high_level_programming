2a10bda1c5c8588222389054763afa270fc200ab
Tosinkuzzy <adeleye608@gmail.com>
2023-11-15 02:03:42 +0100

    logical-operators bugs modified.

--- PROJECT_2/logical-operators.py
+++ PROJECT_2/logical-operators.py
@@ -1,4 +1,4 @@
-''' Logical Operators: and, or, not. We can apply for all types.
+""" Logical Operators: and, or, not. We can apply for all types.
 FOR BOOLEAN TYPES BEHAVIOR
 and -> If both arguments are True then only result is True
 or -> If atleast one argument is True the result is True
@@ -11,7 +11,7 @@ not False -> True
 FOR NON-BOOLEAN TYPES BEHAVIOR
 0 means False
 non-zero means True
-empty string is always trated as False
+empty string is always treated as False
 
 x and y:
 If x is evaluated to x otherwise return y
@@ -23,7 +23,7 @@ If the first argument is zero then result is zero otherwise result is y
 
 x or y:
 If x evaluates to True then result is x otherwise result is y
-'''
+"""
 10 or 20
 0 or 20
 print(10 or 20)
@@ -34,21 +34,21 @@ print(0 or 20)
 print(10 and 20)
 print(0 and 20)
 
-''' not x: If x is evaluated to False then result is True otherwise False '''
+# not x: If x is evaluated to False then result is True otherwise False.
 not 10
 not 0
 print(not 10)
 print(not 0)
 
-# Example
-"mangong" and "mangongsoft"
+# project
+"mangong" and "crownstar"
 "" and "mangong"
-"mangong" and""
+"mangong" and ""
 "" or "mangong"
 "mangong" or ""
 not ""
 not "mangong"
-print("mangong" and "mangongsoft")
+print("mangong" and "crownstar")
 print("" and "mangong")
 print("mangong" and"")
 print("" or "mangong")
