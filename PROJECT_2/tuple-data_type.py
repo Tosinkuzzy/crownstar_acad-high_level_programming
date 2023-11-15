0bd5af016096faf53092d3dc2bc132109318c629
Tosinkuzzy <adeleye608@gmail.com>
2023-11-15 02:32:40 +0100

    code & documentation modified.

--- PROJECT_2/tuple-data_type.py
+++ PROJECT_2/tuple-data_type.py
@@ -1,17 +1,17 @@
-# WAP with the tuple data type which is exactly same as list data type except that it is immutable. i.e we cannot change values.
-# Tuple elements can be represented within parenthesis. E.g
+""" WAP with the tuple data type which is exactly same as list data type except that it is immutable.
+ i.e we cannot change values.
+Tuple elements can be represented within parenthesis. E.g
+"""
 
 t = (10, 20, 30, 40)
-type(t)
-<class 'tuple'>
-t[0] = 100
-TypeError: 'tuple' object does not support item assignment
-t.append("Almighty")
-AttributeError; 'tuple' object has no attribute 'append'
-t.remove(10)
-AttributeError: 'tuple' object has no attribute 'remove'
-print(t[0])
-print(t.append)
-print(t.remove)
+print(type(t))
+#t[0] = 100
+#t.append("Almighty")
+#AttributeError; 'tuple' object has no attribute 'append'
+#t.remove(10)
+#AttributeError: 'tuple' object has no attribute 'remove'
+#print(t[0])
+#print(t.append)
+#print(t.remove)
 
 # NOTE: tuple is the read only version of list
