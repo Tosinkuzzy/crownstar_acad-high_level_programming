c985966a265637e6c1d834324498fab194f4ca66
Tosinkuzzy <adeleye608@gmail.com>
2023-11-15 02:06:19 +0100

    none-data_type modified.

---
+++ PROJECT_2/none-data_type.py
@@ -0,0 +1,8 @@
+""" WAP with no value associated. 
+If the value is not available, then to handle such type of cases None introduced.
+ It is something like null value in Java.
+"""
+def m1():
+    a = 10
+    
+    print(m1)
