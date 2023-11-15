6c0f45f5372dd06d08fc85ffefab899c94a75769
Tosinkuzzy <adeleye608@gmail.com>
2023-11-15 02:19:11 +0100

    set-data_type bugs & documentation.

--- PROJECT_2/set-data_type.py
+++ PROJECT_2/set-data_type.py
@@ -6,10 +6,9 @@
 # e. It is mutable collection
 # f. Growable in nature
 
-# E.g
-s = {100, 0, 10, 200, 10, 'beshley'}
-s # {0, 100, 'beshley', 200, 10}
-s[0] -> TypeError: 'set' object does not support indexing
+s = {100, 0, 10, 200, 10, 'crownstar'}
+s # {0, 100, 'crownstar', 200, 10}
+#s[0] -> TypeError: 'set' object does not support indexing
 
 # set is growable in nature, based on our requirement we can increase or decrease the size.
 
@@ -25,9 +24,9 @@ print(s)
 # Try doing the same code above and print it out this way to see the output
 s.add(60)
 s
-s.remove(100)
+#s.remove(100)
 s
 print(s.add(60))
 print(s)
-print(s.remove(100))
+#print(s.remove(100))
 print(s)
