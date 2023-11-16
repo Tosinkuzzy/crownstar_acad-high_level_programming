d994589c3ffa6b305d2ff976438f38e6c6e043b5
Tosinkuzzy <adeleye608@gmail.com>
2023-11-16 15:45:36 +0100

    wf file modified.

---
+++ PROJECT_2/crap.py
@@ -0,0 +1,17 @@
+wells_fargo = {
+"id": '236',
+"name": 'wells Fargo',
+"address": {
+"city": '4 privet Drive',
+"postalCode": 'GUA',
+"postalPlace": 'Arizona',
+"countryCode": 'United state'
+},
+"line": [
+{
+"phone": '+1 425 673 5563'
+}
+]
+}
+wf = wells_fargo
+print(wf)
