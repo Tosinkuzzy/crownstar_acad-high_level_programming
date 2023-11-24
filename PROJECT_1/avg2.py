7cb55d45c20f42797d85d6eb10ef21f07eee8cea
Tosinkuzzy <adeleye608@gmail.com>
2023-11-24 01:27:27 +0100

    program to compute exam scores modified.

---
+++ PROJECT_1/avg2.py
@@ -0,0 +1,15 @@
+
+"""
+********************HACKBOX******************
+*******CROWNSTAR*****************************
+*******************TOSINKUZZY****************
+"""
+# A program to average two exam scores.
+
+def main():
+	print("This program computes the average of two exam scores.")
+	fr = eval(input("Enter first score: "))
+	sc = eval(input("Enter second score: "))
+	average = (fr + sc) / 2
+	print("The average of the scores is:", average)
+main()
