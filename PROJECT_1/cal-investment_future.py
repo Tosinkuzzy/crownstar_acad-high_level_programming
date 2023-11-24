5102058d13d2dd222a8fb1e2e7a3a4241f422436
Tosinkuzzy <adeleye608@gmail.com>
2023-11-24 03:12:16 +0100

    future investment calculator.

---
+++ PROJECT_1/cal-investment_future.py
@@ -0,0 +1,18 @@
+"""
+*********
+****
+******
+************
+"""
+# A program to compute the value of an investment
+# carried 10years into the future
+
+def main():
+	print("Calculates the future value")
+	print("of a 10-year investment.")
+	principal = eval(input("Enter the initial principal: "))
+	apr = eval(input("Enter the annual interest rate: "))
+	for i in range(10):
+		principal = principal * (1 + apr)
+	print("The value in 10years is:", principal)
+main()
