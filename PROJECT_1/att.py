027c0879916a3061a1255efc41315ade1703940f
Tosinkuzzy <Adeleye608@gmail.com>
2023-11-29 01:35:29 +0100

    Working with faker database, create, manipulaion, insertion, updated

--- PROJECT_1/att.py
+++ PROJECT_1/att.py
@@ -14,17 +14,17 @@ number_of_emails = random.randint(1, 10)
 for _ in range(number_of_emails):
 	# Generate Fname
 	att_first_name = fake.name()
-	ameritech_first_name = fake.name()
-	bellsouth_first_name = fake.name()
-	nvbell_first_name = fake.name()
-	sbcglobal_first_name = fake.name()
+	ameritech_first_name = fake.first_name()
+	bellsouth_first_name = fake.first_name()
+	nvbell_first_name = fake.first_name()
+	sbcglobal_first_name = fake.first_name()
 
 	# Generate Lname
-	att_last_name = fake.name()
-	ameritech_last_name = fake.name()
-	bellsouth_last_name = fake.name()
-	nvbell_last_name = fake.name()
-	sbcglobal_last_name = fake.name()
+	att_last_name = fake.last_name()
+	ameritech_last_name = fake.last_name()
+	bellsouth_last_name = fake.last_name()
+	nvbell_last_name = fake.last_name()
+	sbcglobal_last_name = fake.last_name()
 
 	# Generate a number to mail
 	att = random.randint(1, 24)
@@ -36,8 +36,8 @@ for _ in range(number_of_emails):
 
 	# Generate AT&T email address
 	att_mail = f"{att_first_name}.{att_last_name}{att}@att.net"
-	ameritech_mail = f"{ameritech_first_name{ameritech}}.{last_name}@ameritech.net"
-	bellsouth_mail = f"{bellsouth_first_name}{{bell}bellsouth_last_name}@bellsouth.net"
+	ameritech_mail = f"{ameritech}{ameritech_first_name}.{ameritech_last_name}@ameritech.net"
+	bellsouth_mail = f"{bellsouth_first_name}{bellsouth_last_name}{bell}@bellsouth.net"
 	nvbell_mail = f"{nvbell_first_name}.{nvbell_last_name}{nvbell}@nvbell.net"
 	sbcglobal_mail = f"{sbcglobal_first_name}{sbc}{sbcglobal_last_name}@sbcglobal.net"
 
