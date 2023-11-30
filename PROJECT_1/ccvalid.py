11396ad4fc06ea8230d3f35b5140f53f4b84a349
Tosinkuzzy <Adeleye608@gmail.com>
2023-11-30 01:48:16 +0100

    credit card validator

--- PROJECT_1/ccvalid.py
+++ PROJECT_1/ccvalid.py
@@ -1,8 +1,9 @@
 """
 *****Luhn Algorithm: This algorithm checks the validity of credit card numbers. 
-Here is a Python code snippet that uses the Luhn Algorithm to validate credit card numbers:
+Here is a Python code that uses the Luhn Algorithm to validate credit card numbers:
 *******
 """
+print("CREDIT CARD VALIDATION LOADING.....")
 
 def luhn_check(card_number):
     def digits_of(n):
@@ -12,8 +13,11 @@ def luhn_check(card_number):
     check_sum = sum(nums[::-2] + [sum(divmod(d * 2, 10)) for d in nums[-2::-2]])
     return check_sum % 10 == 0
 
-card_number = "4532015112830361"
+card_number = int(input("ENTER CARD NUMBER: "))
+status = print("VALIDATING CARD...")
 if luhn_check(card_number):
-    print("The credit card number is valid.")
+    print("THE CREDIT CARD NUMBER IS VALID.")
 else:
-    print("The credit card number is not valid.")
+    print("THE CREDIT CARD NUMBER IS NOT VALID.")
+
+print("DONE!")
