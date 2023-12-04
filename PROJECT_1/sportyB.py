b89c728180c10c362032134500311a075cc025c0
Tosinkuzzy <Adeleye608@gmail.com>
2023-12-04 01:49:43 +0100

    sport player, country, and credit

--- PROJECT_1/sportyB.py
+++ PROJECT_1/sportyB.py
@@ -1,23 +1,32 @@
-"""
-virtual G
-"""
-
 import random
+import string
 
-def generate_player_name():
-    return ''.join(random.choice(string.ascii_uppercase) for _ in range(5))
+# Constants
+COUNTRIES = [
+    'USA', 'UK', 'Germany', 'France', 'Spain', 'Italy',
+    'Netherlands', 'Brazil', 'Argentina', 'Australia'
+]
+
+# Functions
+def generate_player_name(length=5):
+    """Generate a random player name consisting of uppercase letters."""
+    return ''.join(random.choice(string.ascii_uppercase) for _ in range(length))
 
 def generate_player_country():
-    countries = ['USA', 'UK', 'Germany', 'France', 'Spain', 'Italy', 'Netherlands', 'Brazil', 'Argentina', 'Australia']
-    return random.choice(countries)
+    """Randomly select a player's country from a predefined list."""
+    return random.choice(COUNTRIES)
 
 def generate_virtual_game_info():
+    """Generate virtual game information for a player."""
     player_name = generate_player_name()
     player_country = generate_player_country()
     player_credits = random.randint(1000, 10000)
     return player_name, player_country, player_credits
 
-player_name, player_country, player_credits = generate_virtual_game_info()
-print("Player Name:", player_name)
-print("Player Country:", player_country)
-print("Player Credits:", player_credits)
+# Main execution
+if __name__ == "__main__":
+    player_name, player_country, player_credits = generate_virtual_game_info()
+    print(f"Player Name: {player_name}")
+    print(f"Player Country: {player_country}")
+    print(f"Player Credits: {player_credits}")
+
