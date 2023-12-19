60e67cb2be99489a8416a4611b8be6387c030605
Tosinkuzzy <Adeleye608@gmail.com>
2023-12-20 00:30:42 +0100

    dollar change counter modified.

--- PROJECT_3/dollar_change.py
+++ PROJECT_3/dollar_change.py
@@ -7,14 +7,14 @@ A PENNIES IS WORTH 1.
 """
 
 def main():
-    print("Change Counter")
+    print("Dollar Change Counter")
     print()
-    print("PLEASE ENTER THE COUNT OF EACH COIN TYPE.")
+    print("Please enter the count OF each coin type.")
     quarters = eval(input("Quarters: "))
     dimes = eval(input("Dimes: "))
     nickels = eval(input("NIckels: "))
     pennies = eval(input("Pennies: "))
     total = quarters * .25 + dimes * .10 + nickels * .05 + pennies *.01
     print()
-    print("THE TOTAL VALUE OF YOUR CHANGE IS ", total)
+    print("The total value of your change is", total)
 main()
