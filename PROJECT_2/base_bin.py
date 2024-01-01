a4ed4d5832a45c09e3d34fedf1165d4b56600861
Tosin Ayomide Adeleye <Adeleye608@gmail.com>
2024-01-01 22:47:54 +0100

    Update base_bin.py

--- PROJECT_2/base_bin.py
+++ PROJECT_2/base_bin.py
@@ -1,4 +1,4 @@
-"""  WAF that can use bin() to convert from any base to binary, 
+"""WAF that can use bin() to convert from any base to binary, 
 When we run this code, the output will not be printed to the console.
 """
 a = bin(15)
@@ -8,7 +8,7 @@ b = bin(0o11)
 c = bin(0X10)
 '0b10000'
 
-# Here is another way to write our code which will get the output printed to the console.
+#Here is another way to write our code which will get the output printed to the console.
 a = bin(15)
 b = bin(0o11)
 c = bin(0X10)
@@ -16,12 +16,10 @@ print(a)
 print(b)
 print(c)
 
-# With the code written below, you will discover that if you run it the output will be printed in the console because we have used the command print.
+#With the code written below, you will discover that if you run it the output will be printed in the console because we have used the command print.
 a = bin(15)
 print('0b1111')
 b = bin(0o11)
 print('0b1001')
 c = bin(0X10)
 print('0b10000')
-
-
