9d7d5685ebdbd9b10249a3e41629a9052de97d43
Tosin Ayomide Adeleye <Adeleye608@gmail.com>
2024-01-01 22:48:49 +0100

    Update base_hex.py

--- PROJECT_2/base_hex.py
+++ PROJECT_2/base_hex.py
@@ -1,9 +1,9 @@
-""" WAP that uses hex() to convert from any base to hexa decimal. """
-# you can also implement by using ***********________********* to design comment, to make your code looks like prof.
-""" If you look at my previous codes with bin() you will see that I used three other methods to come out with the same code.
- You can refer to the previous code and implement here.
+"""WAP that uses hex() to convert from any base to hexa decimal."""
+you can also implement by using ***********________********* to design comment, to make your code looks like prof.
+"""If you look at my previous codes with bin() you will see that I used three other methods to come out with the same code.
+You can refer to the previous code and implement here.
 """
-# your single comment shouldn't be that long ok. if you want to use a long one then implement double quotes
+"""your single comment shouldn't be that long ok. if you want to use a long one then implement double quotes."""
 m = hex(100)
 n = hex(0B111111)
 o = hex(0o12345)
