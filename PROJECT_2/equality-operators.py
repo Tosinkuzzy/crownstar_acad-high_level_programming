807a7e687fb31876ad72436cf696473983bd7963
Tosin Ayomide Adeleye <Adeleye608@gmail.com>
2024-01-01 22:57:13 +0100

    Update equality-operators.py

--- PROJECT_2/equality-operators.py
+++ PROJECT_2/equality-operators.py
@@ -1,4 +1,5 @@
-# Equality Operators: ==, !=. We can apply these operators for any type even for incompatible types also.
+"""Equality Operators: ==, !=. 
+We can apply these operators for any type even for incompatible types also."""
 10 == 20
 10 != 20
 10 == True
@@ -12,7 +13,10 @@ print(False == False)
 print("mangong" == "mangong")
 print(10 == "mangong")
 
-# N.B Changing concept is applicable for equality operators. If at least one comparison returns False then the result is False. Otherwise the result is True.
+"""N.B Changing concept is applicable for equality operators. 
+If at least one comparison returns False then the result is False. 
+Otherwise the result is True.
+"""
 
 10 == 20 == 30 == 40
 10 == 10 == 10 == 10
