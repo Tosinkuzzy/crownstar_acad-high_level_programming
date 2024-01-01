c7d47475dc1fcc0c2e2b8d9b7cf6239aa0b1298a
Tosin Ayomide Adeleye <Adeleye608@gmail.com>
2024-01-01 22:57:55 +0100

    Update escape-characters.py

--- PROJECT_2/escape-characters.py
+++ PROJECT_2/escape-characters.py
@@ -1,4 +1,4 @@
-""" WAP where in string literals we can use escape characters to associate a special meaning """
+"""WAP where in string literals we can use escape characters to associate a special meaning."""
 s = "mangong\nprogrammer"
 print(s)
 s = "Tosinkuzzy\tprogrammer"
@@ -6,4 +6,3 @@ print(s)
 #s = "This is " crownstar" _> error
 s = "This is \" crownstar"
 print(s)
-
