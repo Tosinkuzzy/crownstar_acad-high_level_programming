0ed40e89fc74c3e26233c63c57c6a736fc53edc3
Tosin Ayomide Adeleye <Adeleye608@gmail.com>
2024-01-01 22:47:08 +0100

    Update assignment-operator.py

--- PROJECT_2/assignment-operator.py
+++ PROJECT_2/assignment-operator.py
@@ -1,9 +1,9 @@
-""" Assignment Operators:
+"""Assignment Operators:
 a) We can use this to assign value to the variable. E.g x = 10
 b) We can combine assignment operator with some other operator to form compound assignment operator. 
 E.G x += 10 
 LIST OF POSSIBLE COMPOUND ASSIGNMENT OPERATORS.
-+=, -=, /=, %=, //=, **=, &=, |=, ^=, >>=, <<= """
++=, -=, /=, %=, //=, **=, &=, |=, ^=, >>=, <<=."""
 
 x = 10
 x += 20
