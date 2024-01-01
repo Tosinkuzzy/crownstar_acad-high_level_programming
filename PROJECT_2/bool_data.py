b410c8738d443f98a3faf0d6dea60fdab2358ba9
Tosin Ayomide Adeleye <Adeleye608@gmail.com>
2024-01-01 22:53:20 +0100

    Update bool_data.py

--- PROJECT_2/bool_data.py
+++ PROJECT_2/bool_data.py
@@ -1,12 +1,14 @@
-# WAP that uses boolean or bool data type. The only allowed values for this type of data are True and False. True = 1, False = 0
+"""WAP that uses boolean or bool data type. 
+The only allowed values for this type of data are True and False. True = 1, False = 0
+"""
 a = True
 print(a)
 
-# And to note this point
+"""And to note this point"""
 b = True
 type(b) #=> bool
 
-# See code below. Its output will be True
+"""See code below. Its output will be True"""
 x = 10
 y = 20
 z = x < y
