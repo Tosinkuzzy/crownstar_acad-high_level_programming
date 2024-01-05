db6518825a63ea9b9beeb2e394240c29c8de46bc
Beri <programmingwithkinyuyyurika@gmail.com>
2024-01-05 20:36:46 +0100

    ternary_operator.py

---
+++ PROJECT_2/ternary_operator.py
@@ -0,0 +1,8 @@
+#Ternary Operator or Conditional Operator
+"""Syntax: x = firstValue if condition else secondValue. If condition is True then firstValue will be considered else secondValue will be considered."""
+#E.g 1:
+
+a, b = 10, 20
+x = 30 if a < b else 40
+
+print(x)
