0f1b90b2a93c938eff485e16afeef9938158f866
Beri <programmingwithkinyuyyurika@gmail.com>
2024-01-06 13:03:15 +0100

    functions_variables.py

---
+++ PROJECT_2/functions_variables.py
@@ -0,0 +1,18 @@
+"""Important Functions of math Module
+ceil(x)
+floor(x)
+pow(x , y)
+factorial(x)
+trunc(x)
+gcd(x , y)
+sin(x)
+cos(x)
+tan(x)
+...
+
+Important Variables of math Module
+
+pi2.14
+e -> 2.71
+inf -> infinity
+nan - > not a number"""
