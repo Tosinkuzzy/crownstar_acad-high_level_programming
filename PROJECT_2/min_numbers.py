963b58dc14daa17cbebe54054671a864172062c8
Beri <programmingwithkinyuyyurika@gmail.com>
2024-01-06 11:36:40 +0100

    min_numbers.py

---
+++ PROJECT_2/min_numbers.py
@@ -0,0 +1,7 @@
+# Program for minimum of 3 numbers
+
+a = int(input("Enter First Number:"))
+b = int(input("Enter Second Number:"))
+c = int(input("Enter Third Number:"))
+min = a if a < b and a < c else b if b < c else c
+print("Minimum Value:", min)
