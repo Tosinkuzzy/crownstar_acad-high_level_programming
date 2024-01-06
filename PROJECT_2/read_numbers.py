6175fc085330d99a1f738f7e2f0f040745c6b818
Beri <programmingwithkinyuyyurika@gmail.com>
2024-01-06 13:39:19 +0100

    read_numbers.py

---
+++ PROJECT_2/read_numbers.py
@@ -0,0 +1,13 @@
+#Write a program to read 2 numbers from the keyboard and print sum
+
+x = input("Enter First Number:")
+y = input("Enter Second Number:")
+i = int(x)
+j = int(y)
+print("The Sum:", i + j)
+
+x = int(input("Enter First Number:"))
+y = int(input("Enter Second Number:"))
+print("The Sum:", x + y)
+
+print("The Sum:", int(input("Enter First Number:"))+int(input("Enter Seconf Number:")))
