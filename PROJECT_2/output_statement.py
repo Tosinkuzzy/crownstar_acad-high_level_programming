f6ba9dce0fd0f7eaa3f360e6d60fadc6c849ff91
Beri <programmingwithkinyuyyurika@gmail.com>
2024-01-07 17:26:50 +0100

    output_statement.py

---
+++ PROJECT_2/output_statement.py
@@ -0,0 +1,12 @@
+""" Output Statements. We can use print() function to display output
+- FORM 1: print() without any argument. Just it prints new line character
+- FORM 2: """
+# Print string
+print("Hello World")
+# We can use escape characters also
+print("Hello \n World")
+print("Hello\tWorld")
+# We can use repetetion operator (*) in the string
+print(10*"Hello")
+# We can use + operator also
+print("Hello"+"World")
