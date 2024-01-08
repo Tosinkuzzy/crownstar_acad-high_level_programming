155b82b37fa2a4fb3a38819eb6cedad5d6b1c20a
Beri <programmingwithkinyuyyurika@gmail.com>
2024-01-08 11:50:55 +0100

    accessing_characters.py

---
+++ accessing_characters.py
@@ -0,0 +1,16 @@
+""" Accessing Characters By using Index:
+- Python supports both +ve and -ve index.
+- +ve index means Left to Right (Forward Direction)
+- -ve index means Right to Left (Backward Direction)
+E.g: s = 'durga'
+
+s = 'durga'
+s[0]
+'d'
+s[4]
+'a'
+s[-1]
+'a'
+s[10] 
+
+NOTE: If we are trying to access characters of a string without of range index then we will get error saying: IndexError"""
