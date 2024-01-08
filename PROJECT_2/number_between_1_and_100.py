5335e2aba74e809055340543e4129e0b62b0e412
Beri <programmingwithkinyuyyurika@gmail.com>
2024-01-08 03:03:35 +0100

    number_between_1_and_100.py

---
+++ PROJECT_2/number_between_1_and_100.py
@@ -0,0 +1,8 @@
+# WAP to check whether the given number is in between 1 and 100?
+
+n1 = int(input("Enter Number:"))
+if n>=1 and n<=10:
+    print("The number",n,"is in between 1 to 10")
+else:
+    print("The number",n,"is not in between 1 to 10")
+    
