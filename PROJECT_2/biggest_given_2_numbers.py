d9d3e3d7e960f7c6dc7adfc0dd21727307f311b4
Beri <programmingwithkinyuyyurika@gmail.com>
2024-01-08 02:51:09 +0100

    biggest_given_2_numbers.py

---
+++ PROJECT_2/biggest_given_2_numbers.py
@@ -0,0 +1,8 @@
+# WAP to find Biggest of given 2 Numbers from the Command Prompt
+
+n1 = int(input("Enter First Number:"))
+n2 = input(input("Enter Second Number:"))
+if n1>n2:
+    print("Biggest Number is:",n1)
+else:
+    print("Biggest Number is:",n2)
