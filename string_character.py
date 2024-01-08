cafb57e4d8ef263c5fcd006a842b0428667fa19c
Beri <programmingwithkinyuyyurika@gmail.com>
2024-01-08 16:45:47 +0100

    string_character.py

---
+++ string_character.py
@@ -0,0 +1,28 @@
+""" WAP to access each character of string in forward and backward direction by using while loop"""
+
+s = "Learning Python is very easy!!!"
+n = len(s)
+i = 0
+print("Forward direction")
+while i < n:
+    print(s[i],end='')
+i += 1
+print("Backward direction")
+i = -1
+while i >= -n:
+    print(s[i],end='')
+    i = i-1
+    
+# Alternative ways:
+
+s = "Learning Python is very easy!!!"
+print("Forward direction")
+for i in s:
+    print(i, end='')
+print("Forward direction")
+for i in s[::]:
+        print(i,end='')
+        
+print("Backward direction")
+for i in s[::-1]:
+    print(i,end='')
