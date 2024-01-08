11182a0935bfa5248c3f2b1c9581887e90162f2f
Beri <programmingwithkinyuyyurika@gmail.com>
2024-01-08 03:11:59 +0100

    single_digits.py

---
+++ PROJECT_2/single_digits.py
@@ -0,0 +1,27 @@
+""" WAP to take a single digit number from the key board and print its value in English Word?
+0 -> ZERO
+1 -> ONE"""
+
+n = int(input("Enter a digit from 0 to 9:"))
+if n==0:
+    print("ZERO")
+elif n==1:
+    print("ONE")
+elif n==2:
+    print("TWO")
+elif n==3:
+    print("THREE")
+elif n==4:
+    print("FOUR")
+elif n==5:
+    print("FIVE")
+elif n==6:
+    print("SIX")
+elif n==7:
+    print("SEVEN")
+elif n==8:
+    print("EIGHT")
+elif n==9:
+    print("NINE")
+else:
+    print("PLEASE ENTER A DIGIT FROM 0 TO 9")
