6923f82e24ca70d10206a3979ac79e86c1c5ac42
Beri <programmingwithkinyuyyurika@gmail.com>
2024-01-08 17:51:57 +0100

    substring_positions.py

---
+++ substring_positions.py
@@ -0,0 +1,15 @@
+# Program to display all positions of substring in a given main string
+
+s = input("Enter main string:")
+subs = input("Enter sub string:")
+flag = False
+pos=-1
+n=len(s)
+while True:
+    pos=s.find(subs,pos+1,n)
+    if pos==-1:
+        break
+print("Found at position",pos)
+flag = True
+if flag==False:
+    print("Not Found")
