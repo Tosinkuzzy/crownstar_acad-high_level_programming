80a462a85289bc584afdbea4ed50ecfcf447c307
Tosin Ayomide Adeleye <Adeleye608@gmail.com>
2024-01-09 05:48:59 +0100

    Update statement_output.py

--- PROJECT_2/statement_output.py
+++ PROJECT_2/statement_output.py
@@ -1,59 +1,71 @@
-""" Output Statements. We can use print() function to display output"""
+""" 
+Output Statements. We can use print() function to display output
+"""
 
-print("Hello"+"World")
+print("Hello" + "World")
 print("Hello","World")
 
-# FORM 3: print() with variable number of arguments
+# print() with variable number of arguments
 
 a, b, c = 10, 20, 30
-print("The Values are : a,b,c")
+print("The Values are : {:d}{:d}{:d}".format(a, b, c))
 
-"""By default output values are separated by space. If we want we can specify separator by using "sep" attribute"""
+"""
+By default output values are separated by space. 
+If we want we can specify separator by using "sep" attribute
+"""
 
 a, b, c = 10, 20, 30
 print(a, b, c, sep = ',')
 print(a, b, c, sep = ':')
 
-# FORM 4: print() with end attribute
+# print() with end attribute
 
 print("Hello")
-print("Durga")
-print("Soft")
+print("Crown")
+print("Star")
 
 # If we want output in the same line with space
 
 print("Hello", end = '')
-print("Durga", end = '')
-print("Soft")
+print("Crown", end = '')
+print("Star")
 
-"""NOTE: The default value for end attribute is \n, which is nothing but new line character"""
+"""
+NOTE: The default value for end attribute is \n, which is nothing but new line character
+"""
 
-# FORM 5: print(object) statement
+# print(object) statement
 
-"""We can pass any object (like list, tuple, set etc) as argument to the print() statement"""
+"""
+We can pass any object (like list, tuple, set etc) as argument to the print() statement
+"""
 
 l = [10, 20, 30, 40]
 t = [10, 20, 30, 40]
 print(l)
 print(t)
 
-# FORM 6: print(string, variable list)
-"""We can use print() statement with String and any number of arguments"""
+# print(string, variable list)
+"""
+We can use print() statement with String and any number of arguments
+"""
 
-s = "Durga"
+s = "Crownstar"
 a = 48
 s1 = "Java"
 s2 = "Python"
 print("Hello", s, "Your Age is", a)
 print("You are teaching", s1, "and", s2)
 
-"""FORM 7: print(formatted string)
+""" print(formatted string)
 
 %i
 %d
 %f
 %s
-Syntax: print("formatted string" %(variable list))"""
+Syntax: print("formatted string" %(variable list))
+"""
 
 # E.g 1:
 
@@ -65,14 +77,14 @@ print("b value is %d and c value is %d" %(b,c))
 
 # E.g 2:
 
-s = "Durga"
+s = "Crownstar"
 list = [10, 20, 30, 40]
 print("Hello %s ... The List of items are %s" %(s,list))
 
 # FORM 8: print() with replacement operator () E.g:
 
-name = "Durga"
+name = "Crownstar"
 salary = 10000
-gf = "Sunny"
-print("Hello {0} your salary is {1} and Your Friend {2} is waiting". format(name, salary, gf))
-print("Hello {x} your salary is {y} and Your Friend {z} is waiting". format(x=name, y=salary, z=gf))
+gf = "Kuzzy"
+print("Hello {0} your salary is {1} and Your Friend {2} is waiting".format(name, salary, gf))
+print("Hello {x} your salary is {y} and Your Friend {z} is waiting".format(x=name, y=salary, z=gf))
