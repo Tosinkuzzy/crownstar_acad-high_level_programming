de6f5fe3492bf0511122a1ad5afe44dc9cf07b4e
Tosin Ayomide Adeleye <Adeleye608@gmail.com>
2024-01-09 05:40:33 +0100

    Update case_of_pass.py

--- PROJECT_2/case_of_pass.py
+++ PROJECT_2/case_of_pass.py
@@ -2,7 +2,8 @@
 Using Case of Pass:
 Sometimes in the parent class we have to declare a function with empty body and child class responsible to provide proper implementation. 
 Such type of empty body we can define by using pass keyword. (It is something like abstract method in Java)
-E.g: def m1(): pass"""
+E.g: def m1(): pass
+"""
 
 for i in range(100):
     if i % 9 ==0:
