b49469f9c39b46844422a2c3e546fd9c203fe400
Tosinkuzzy <Adeleye608@gmail.com>
2024-04-06 04:22:34 +0100

    working on files

--- PROJECT_5/operator_casestudy.py
+++ PROJECT_5/operator_casestudy.py
@@ -1,21 +1,23 @@
+#!/usr/bin/env python3
+
 # Slice Operator Case Study:
 s = 'abcdefghij'
-s[1:6:2]
-s[::1]
-s[::-1]
-s[3:7:-1]
-s[7:4:-1]
-s[0:10000:1]
-s[-4:1:-1]
-s[-4:1:-2]
-s[5:0:1]
+print(s[1:6:2])
+print(s[::1])
+print(s[::-1])
+print(s[3:7:-1])
+print(s[7:4:-1])
+print(s[0:10000:1])
+print(s[-4:1:-1])
+print(s[-4:1:-2])
+print(s[5:0:1])
 #s[9:0:0]
-s[0:-10:-1]
-s[0:-11:-1]
-s[0:0:1]
-s[0:-9:-2]
-s[-5:-9:-2]
-s[10:-1:-1]
-s[10000:2:-1]
+print(s[0:-10:-1])
+print(s[0:-11:-1])
+print(s[0:0:1])
+print(s[0:-9:-2])
+print(s[-5:-9:-2])
+print(s[10:-1:-1])
+print(s[10000:2:-1])
 
 # NOTE: Slice operator never raises IndexError
