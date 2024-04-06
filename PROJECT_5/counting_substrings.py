e1f6b9cd4e547ddb0b4951d71ca6477f27a84002
Tosinkuzzy <Adeleye608@gmail.com>
2024-04-06 03:58:34 +0100

    working on files

--- PROJECT_5/counting_substrings.py
+++ PROJECT_5/counting_substrings.py
@@ -1,7 +1,11 @@
-""" COUNTING SUBSTRING IN THE GIVEN STRING:
+#!/usr/bin/env python3
+
+""" 
+COUNTING SUBSTRING IN THE GIVEN STRING:
 We can find the number of occurrences of substring present in the given string by using count() method.
 - s.count(substring) -> It will search through out the string.
-- s.count(substring, bEgin, end) -> It will search from bEgin index to end-1 index."""
+- s.count(substring, bEgin, end) -> It will search from bEgin index to end-1 index.
+"""
 
 s = "abcabcabcabcadda"
 print(s.count('a'))
