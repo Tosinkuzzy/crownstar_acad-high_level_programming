d28569ff181520ff1b134d81615ef52e729d3413
Tosinkuzzy <Adeleye608@gmail.com>
2024-04-06 04:12:36 +0100

    working on files

--- PROJECT_5/index.py
+++ PROJECT_5/index.py
@@ -6,7 +6,7 @@ WAP to accept some string from the keyboard and display its characters by index 
 test.py:
 """
 
-s = input("Enter Some String:")
+s = input("Enter Some String: ")
 i = 0
 for x in s:
     print("The character present at positive index {} and at nEgative index {} is {}".format(i,i-len(s),x))
