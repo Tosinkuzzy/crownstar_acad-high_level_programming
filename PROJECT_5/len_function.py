a5063d6a57c2a2505c8fe35e166ffdfa56e126a5
Tosinkuzzy <Adeleye608@gmail.com>
2024-04-06 04:16:46 +0100

    working on files

--- PROJECT_5/len_function.py
+++ PROJECT_5/len_function.py
@@ -1,6 +1,10 @@
-"""len() in-built function:
-we can use len() function to find the number of characters present in the string.
-E.g: """
+#!/usr/bn/env python3
 
-s = 'durga'
+"""
+len() in-built function:
+we can use len() function to find the number of characters present in the string.
+E.g:
+"""
+
+s = 'crownstar'
 print(len(s))
