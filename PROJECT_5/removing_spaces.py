80da65f978723344bf2987359a97160cd80074ce
Tosinkuzzy <Adeleye608@gmail.com>
2024-04-06 04:25:36 +0100

    working on files

--- PROJECT_5/removing_spaces.py
+++ PROJECT_5/removing_spaces.py
@@ -1,16 +1,20 @@
-""" Removing Spaces from the string:
+#!/usr/bin/env python3
+
+""" 
+Removing Spaces from the string:
 We can use the following 3 methods
 - rstrip() -> To remove spaces at right hand side
 lstrip() -> To remove spaces at left hand side
-strip() -> To remove spaces both sides"""
+strip() -> To remove spaces both sides
+"""
 
-city = input("Enter your city Name:")
+city = input("Enter your city Name: ")
 scity = city.strip()
-if scity=='Hyderabad':
-    print("Hello Hyderbadi..Adab")
-elif scity=='Chennai':
-    print("Hello Madrasi...Vanakkam")
-elif scity=="Bangalore":
-    print("Hello Kannadiga...Shubhodaya")
+if scity=='Abuja':
+    print("Hello Abuja..Beri")
+elif scity=='Lagos':
+    print("Hello Lagos...Tosinkuzzy")
+elif scity=="Ondo":
+    print("Hello Ondo...Toy")
 else:
     print("Your entered city is invalid")
