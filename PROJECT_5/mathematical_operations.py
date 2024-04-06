84ce005fa6f0316d665a21a53ec829a4edc88e0d
Tosinkuzzy <Adeleye608@gmail.com>
2024-04-06 04:18:56 +0100

    working on files

--- PROJECT_5/mathematical_operations.py
+++ PROJECT_5/mathematical_operations.py
@@ -1,11 +1,17 @@
-"""Mathematical Operators for string:
-    we can apply the following mathematical operators for strings
+#!/usr/bin/env python3
+
+"""
+Mathematical Operators for string:
+we can apply the following mathematical operators for strings
 - + operator for concatenation
-- * operator for repetition """
+- * operator for repetition 
+"""
 
-print("durga"+"soft")
-print("durga"*2)
+print("durga" + "soft")
+print("durga" *2)
 
-""" NOTE:
+""" 
+NOTE:
 1) To use + operator for strings, compulsory both arguments should be str type.
-2) To use * operator for strings, compulsory one argument should  be str and other argument should be int."""
+2) To use * operator for strings, compulsory one argument should  be str and other argument should be int.
+"""
